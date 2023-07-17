f35060ff61c4ff81fa1547fa2683e90df9d7b2a7
Kapee87 <kapserker@gmail.com>
2023-07-17 17:29:53 -0300

    agregado menu dropdown

--- scripts/index.js
+++ scripts/index.js
@@ -7,24 +7,30 @@ let navLinks = document.querySelectorAll('header ul li a')
 window.onscroll = () => {
     sections.forEach(section => {
         let top = window.scrollY;
-        let offset = section.offsetTop - 150;
+        let offset = section.offsetTop - 120;
         let height = section.offsetHeight;
         let id = section.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
+                document.querySelector('#logo').classList.remove('active')
                 document.querySelector('header ul li a[href*=' + id + ']').classList.add('active')
             })
+        } else if (top < 600) {
+            document.querySelector('#logo').classList.add('active')
         }
     })
 }
 
 /* 
-Botón hamburguesa
+Botón hamburguesa y menú dropdown
 */
 const burgerBtn = document.getElementById('burger')
+const navBar = document.querySelector('#navbar>ul')
+
 burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('open')
+    navBar.classList.toggle('dropdown')
 })
 
